3d08c9f35a56f893e616dcc841f46611f562d78b
David Hankin <d_hankin@outlook.com>
2024-01-04 13:44:16 +0100

    grundklasser

---
+++ logingrupp5/src/main/java/com/logingrupp5/logingrupp5/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.logingrupp5.logingrupp5.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+    
+}
---
+++ logingrupp5/src/main/java/com/logingrupp5/logingrupp5/service/JpaUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.logingrupp5.logingrupp5.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.logingrupp5.logingrupp5.repository.UserRepository;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public JpaUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        
+
+        throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+    }
+    
+}
