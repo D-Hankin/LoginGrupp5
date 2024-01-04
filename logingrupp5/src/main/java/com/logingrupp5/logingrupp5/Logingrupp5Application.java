d0d42306a125fe0dbd19d43df003d534a7c4d976
David Hankin <d_hankin@outlook.com>
2024-01-04 10:10:24 +0100

    Spring Boot ramverk

---
+++ logingrupp5/src/main/java/com/logingrupp5/logingrupp5/Logingrupp5Application.java
@@ -0,0 +1,13 @@
+package com.logingrupp5.logingrupp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Logingrupp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Logingrupp5Application.class, args);
+	}
+
+}
---
+++ logingrupp5/src/test/java/com/logingrupp5/logingrupp5/Logingrupp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logingrupp5.logingrupp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Logingrupp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
