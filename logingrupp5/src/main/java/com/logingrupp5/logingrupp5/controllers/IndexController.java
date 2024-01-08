2608262c77509b913329e466ecfea030bf67d1b3
Kurvel <143402591+Kurvel@users.noreply.github.com>
2024-01-08 17:31:41 +0100

    WIP bilder på indexsidan

--- logingrupp5/src/main/java/com/logingrupp5/logingrupp5/controllers/IndexController.java
+++ logingrupp5/src/main/java/com/logingrupp5/logingrupp5/controllers/IndexController.java
@@ -1,24 +1,36 @@
 package com.logingrupp5.logingrupp5.controllers;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+import com.logingrupp5.logingrupp5.model.Product;
+import com.logingrupp5.logingrupp5.repository.ProductRepository;
+
 @Controller
 public class IndexController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    Product prod = new Product();
+    
     @GetMapping("/")
     public String index(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if(authentication != null && authentication.isAuthenticated()) {
-            String username = authentication.getName();
-            model.addAttribute("username", username);
-        } else {
-            model.addAttribute("username", "inte inloggad");
-        }
+         model.addAttribute("products", productRepository.findAll());
+        
+
+        
+       
+        
         return "index";
     }
+
+    
 }
