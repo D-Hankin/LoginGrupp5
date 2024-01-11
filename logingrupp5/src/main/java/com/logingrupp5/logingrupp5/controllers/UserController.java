66023959d9522c5f268252bf62c1c689a0e77b39
David Hankin <d_hankin@outlook.com>
2024-01-11 10:36:10 +0100

    register without login in link

--- logingrupp5/src/main/java/com/logingrupp5/logingrupp5/controllers/UserController.java
+++ logingrupp5/src/main/java/com/logingrupp5/logingrupp5/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.logingrupp5.logingrupp5.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
